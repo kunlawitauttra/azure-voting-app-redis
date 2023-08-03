bd06a9ab5be64c21cdcadc04d100f3b657e19c8a
kunlawit <74113723+kunlawitauttra@users.noreply.github.com>
2023-08-04 02:08:12 +0700

    Update main.py

--- azure-vote/azure-vote/main.py
+++ azure-vote/azure-vote/main.py
@@ -1,88 +1,78 @@
 from flask import Flask, request, render_template
 import os
-import random
-import redis
+import pymssql
 import socket
-import sys
 
 app = Flask(__name__)
 
 # Load configurations from environment or config file
 app.config.from_pyfile('config_file.cfg')
 
-if ("VOTE1VALUE" in os.environ and os.environ['VOTE1VALUE']):
-    button1 = os.environ['VOTE1VALUE']
-else:
-    button1 = app.config['VOTE1VALUE']
-
-if ("VOTE2VALUE" in os.environ and os.environ['VOTE2VALUE']):
-    button2 = os.environ['VOTE2VALUE']
-else:
-    button2 = app.config['VOTE2VALUE']
-
-if ("TITLE" in os.environ and os.environ['TITLE']):
-    title = os.environ['TITLE']
-else:
-    title = app.config['TITLE']
-
-# Redis configurations
-redis_server = os.environ['REDIS']
-
-# Redis Connection
-try:
-    if "REDIS_PWD" in os.environ:
-        r = redis.StrictRedis(host=redis_server,
-                        port=6379,
-                        password=os.environ['REDIS_PWD'])
-    else:
-        r = redis.Redis(redis_server)
-    r.ping()
-except redis.ConnectionError:
-    exit('Failed to connect to Redis, terminating.')
+# Azure SQL Database configurations
+server = 'test-failover-group.database.windows.net'
+username = 'testadmin'
+password = '6yHnmju&'
+database = 'test-sg-db'
 
 # Change title to host name to demo NLB
 if app.config['SHOWHOST'] == "true":
     title = socket.gethostname()
 
-# Init Redis
-if not r.get(button1): r.set(button1,0)
-if not r.get(button2): r.set(button2,0)
+# Connect to Azure SQL Database
+try:
+    conn = pymssql.connect(server=server, user=username, password=password, database=database)
+except pymssql.Error as e:
+    exit(f'Failed to connect to Azure SQL Database: {e}')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
 
     if request.method == 'GET':
 
-        # Get current values
-        vote1 = r.get(button1).decode('utf-8')
-        vote2 = r.get(button2).decode('utf-8')            
+        # Get current values from the database
+        vote1, vote2 = get_votes_from_db()
 
         # Return index with values
-        return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
+        return render_template("index.html", value1=vote1, value2=vote2, button1=button1, button2=button2, title=title)
 
     elif request.method == 'POST':
 
         if request.form['vote'] == 'reset':
-            
-            # Empty table and return results
-            r.set(button1,0)
-            r.set(button2,0)
-            vote1 = r.get(button1).decode('utf-8')
-            vote2 = r.get(button2).decode('utf-8')
-            return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
-        
-        else:
+            # Empty the votes in the database
+            reset_votes_in_db()
 
+        else:
             # Insert vote result into DB
             vote = request.form['vote']
-            r.incr(vote,1)
-            
-            # Get current values
-            vote1 = r.get(button1).decode('utf-8')
-            vote2 = r.get(button2).decode('utf-8')  
-                
-            # Return results
-            return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
+            add_vote_to_db(vote)
+
+    # Get current values from the database
+    vote1, vote2 = get_votes_from_db()
+
+    # Return results
+    return render_template("index.html", value1=vote1, value2=vote2, button1=button1, button2=button2, title=title)
+
+def get_votes_from_db():
+    # Get the current values of votes from the database
+    cursor = conn.cursor()
+    cursor.execute(f"SELECT VoteCount FROM Votes WHERE VoteOption = '{button1}'")
+    vote1 = cursor.fetchone()[0]
+    cursor.execute(f"SELECT VoteCount FROM Votes WHERE VoteOption = '{button2}'")
+    vote2 = cursor.fetchone()[0]
+    return int(vote1), int(vote2)
+
+def reset_votes_in_db():
+    # Reset votes in the database
+    cursor = conn.cursor()
+    cursor.execute(f"UPDATE Votes SET VoteCount = 0 WHERE VoteOption = '{button1}'")
+    cursor.execute(f"UPDATE Votes SET VoteCount = 0 WHERE VoteOption = '{button2}'")
+    conn.commit()
+
+def add_vote_to_db(vote):
+    # Insert a new vote into the database
+    cursor = conn.cursor()
+    cursor.execute(f"UPDATE Votes SET VoteCount = VoteCount + 1 WHERE VoteOption = '{vote}'")
+    conn.commit()
 
 if __name__ == "__main__":
     app.run()
